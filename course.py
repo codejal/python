051ecc9d80f5eae9787c78d31e5b0fe8a82eeeea
pranjal <pranjalg2501@gmail.com>
2020-04-17 00:00:56 +0530

    Create course.py

---
+++ course.py
@@ -0,0 +1,15 @@
+st1 = "hell"
+st2 = "hell"
+st3 = st1+st2
+print(st3)
+x = '40'
+y = int(x) + 2
+print(y)
+print(len('banana')*7)
+data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
+pos = data.find('.')
+print(data[pos:pos+3])
+
+friends = ['Joseph', 'Glenn', 'Sally']
+friends.sort()
+print(friends[0])
