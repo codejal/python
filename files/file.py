7fe4bfdb64d28d765dd0627eb282f51f7623fcf9
pranjal <pranjalg2501@gmail.com>
2020-04-25 22:48:22 +0530

    Create file.py

---
+++ files/file.py
@@ -0,0 +1,11 @@
+fhandle = open("random.txt", "r")
+count = 0
+for sentence in fhandle:
+    count = count+1
+    sentence = sentence.lstrip()
+    print(sentence)
+fhandle = open("random.txt", "r")
+whole = fhandle.read()
+print(len(whole))
+print(count)
+print(whole.lstrip())
