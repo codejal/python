dd61c07054ccec8097100089da5a3630b5c2a8ad
pranjal <pranjalg2501@gmail.com>
2020-05-05 21:07:43 +0530

    Create soclet.py

---
+++ web_data/soclet.py
@@ -0,0 +1,17 @@
+import socket
+mysoc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysoc.connect(('data.pr4e.org', 80))
+cm = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysoc.send(cm)
+count = 0
+while True:
+    # 512 is the amount of cycle recieved in every cycle
+    data = mysoc.recv(512)
+    if (len(data) < 1):
+        break
+    print(data.decode())
+    count += 1
+mysoc.close()
+# we know that 512 is the datat we recieve in one cycle
+# ALSO the file we aare recieve has 536 bytes therefor count will be according
+print(count)
