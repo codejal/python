2267b71329c139328f38683f439185c97ba7ee64
pranjal <pranjalg2501@gmail.com>
2020-05-05 14:56:56 +0530

    Update regular_exp.py

--- web_data/regular_exp.py
+++ web_data/regular_exp.py
@@ -1,1 +1,11 @@
 import re
+lin = 'my name is pranjal@33321.ert.75.f is the best'
+y = re.findall('.*', lin)
+print(y)
+y = re.findall('@([^ ]*)', lin)
+print(y)
+# starting with my having  @ extract sigle non blank character zero or more times
+y = re.findall('^my @(\S+)', lin)
+print(y)
+y = re.findall('\S+?@\S+?', lin)
+print(y)
