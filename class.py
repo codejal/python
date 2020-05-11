aff566cbfa9a379f598ac56e5aadacb2037e0c2a
pranjal <pranjalg2501@gmail.com>
2020-05-11 21:36:21 +0530

    Create class.py

---
+++ class.py
@@ -0,0 +1,14 @@
+# creating classes
+class Point:
+    def __init__(self):
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        print("draw")
+
+    def distance(self, x, y):
+        print((x ^ 2+y ^ 2) ^ 0.5)
+
+
+point = Point(1, 4)
